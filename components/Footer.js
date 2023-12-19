38ecfd8516ff9f36866fcc52e8204b51879c9700
Rafael dos Santos <rspwebdesigner@gmail.com>
2023-12-19 01:57:03 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        [![Netlify Status](https://api.netlify.com/api/v1/badges/ae1d80e8-137f-46b0-9b0d-68b58d7fa00e/deploy-status)](https://app.netlify.com/sites/auditeste-rsp/deploys)
+        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
