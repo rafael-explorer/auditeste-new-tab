5e4d2f47e1df87b99afe02914f6a9d0d218f26f7
Rafael dos Santos <rspwebdesigner@gmail.com>
2023-12-19 01:52:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Auditeste - Rafael dos Santos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Olá, Rafael!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Pronto pra mais um dia de trabalho?
         </p>
       </main>
 
